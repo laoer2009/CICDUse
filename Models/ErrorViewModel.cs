f2080b74de8d0f6b27c5df2a7f03f6c0b1923f82
licl <licl@LICHUNLI>
2021-04-20 20:35:25 +0800

    增加字段

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -7,5 +7,7 @@ namespace CICDUse.Models
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        public DateTime dtNow { get; set; }
     }
 }
